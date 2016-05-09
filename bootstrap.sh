869d8e834ba2d5877ed06c8d7ffb5952a99d47aa
mwpolcik <mwpolcik@gmail.com>
2016-05-09 14:33:32 +0200

    added line to set server name

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,7 @@ apt-get update
 apt-get install -y apache2
 if ! [ -L /var/www ]; then
     rm -rf /var/www
-    ln -s /vagrant /var/www
+    ln -s /vagrant/html /var/www
+    echo "ServerName localhost" | sudo tee /etc/apache2/httpd.conf 
+    sudo service apache2 restart
 fi
